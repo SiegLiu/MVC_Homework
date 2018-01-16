90607e15e9c9183ed430d100062f83cb83c6c2bf
SiegLiu <yuchih.L@gmail.com>
2018-01-17 01:53:09 +0800

    加入帳本中文標題

--- MVC_Homework/Models/ViewModels/AccountViewModel.cs
+++ MVC_Homework/Models/ViewModels/AccountViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,8 +8,13 @@ namespace MVC_Homework.Models.ViewModels
 {
     public class AccountViewModel
     {
+        [Display(Name = "類別")]
         public string category { get; set; }
+
+        [Display(Name = "日期")]
         public DateTime billdate { get; set; }
+
+        [Display(Name = "金額（元）")]
         public int amount { get; set; }
     }
 }
