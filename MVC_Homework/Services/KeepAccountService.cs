5da4307c2262544351f7484127549e881e95bbb7
SiegLiu <yuchih.L@gmail.com>
2018-01-25 02:22:49 +0800

    加入Service層

---
+++ MVC_Homework/Services/KeepAccountService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MVC_Homework.Models;
+using MVC_Homework.Models.ViewModels;
+
+namespace MVC_Homework.Services
+{
+    public class KeepAccountService
+    {
+        private readonly AccountBookDAO _dao;
+        public KeepAccountService()
+        {
+            _dao = new AccountBookDAO();
+        }
+
+        public void Add (AccountViewModel accountbook)
+        {
+            accountbook.billdate = DateTime.Now;
+            _dao.Insert(accountbook);
+        }
+
+        public IEnumerable<AccountViewModel> GetAllAccount()
+        {
+            return _dao.GetAllAccount();
+        }
+    }
+}
