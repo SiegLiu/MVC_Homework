399da11b2a2d8760099dd90bb8b55c8833dd9037
SiegLiu <yuchih.L@gmail.com>
2018-02-01 02:34:41 +0800

    加入驗證-所有欄位必填、「金額」只能輸入正整數，「日期」不得大於今天、「備註」最多輸入100個字元（備註為 TextArea）

---
+++ MVC_Homework/Controllers/ValidController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Homework.Controllers
+{
+    public class ValidController : Controller
+    {
+        // GET: Valid
+        public ActionResult ValidateDateLessOrEqual(DateTime Billdate)
+        {
+            bool isValidate = Billdate <= DateTime.Now;
+            return Json(isValidate, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
--- MVC_Homework/Models/ViewModels/AccountViewModel.cs
+++ MVC_Homework/Models/ViewModels/AccountViewModel.cs
@@ -1,22 +1,32 @@
-﻿using System;
+﻿using MVC_Homework.Filters;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace MVC_Homework.Models.ViewModels
 {
     public class AccountViewModel
     {
+        [Required]
         [Display(Name = "類別")]
         public string Category { get; set; }
 
+        [Required]
+        [Range(1, int.MaxValue)]
         [Display(Name = "金額（元）")]
         public decimal Amount { get; set; }
 
+        [Required]
+        [DataType(DataType.Date)]
+        [RemotePlus("ValidateDateLessOrEqual", "Valid", "", ErrorMessage = "「日期」不得大於今天")]
         [Display(Name = "日期")]
         public DateTime Billdate { get; set; }
 
+        [Required]
+        [StringLength(100, ErrorMessage = "「備註」最多輸入100個字元。", MinimumLength = 1)]
         [Display(Name = "備註")]
         public string Remark { get; set; }
     }
